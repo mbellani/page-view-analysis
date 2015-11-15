1075c966ff6a4a63d4dd037a769753b2ad1257c6
Manish Bellani <manish.bellani@gmail.com>
2015-11-15 15:18:52 -0600

    refactored PageViewsByLanguage so that context is not passed to it.  - ran into task serialization issues.  - apparently i don't know something about how spark task serialization works.  - there's probably a better way of structuring a spark program, i just don't know it yet.

--- src/main/scala/com/nbs/PageViewAnalysis.scala
+++ src/main/scala/com/nbs/PageViewAnalysis.scala
@@ -1,13 +1,26 @@
 package com.nbs
 
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{DataFrame, SQLContext}
 import org.apache.spark.{SparkConf, SparkContext}
 
 object PageViewAnalysis {
 
   def main(args: Array[String]) {
-    val sc = new SparkContext(conf)
-    val views = new PageViewsByLanguage(sc).aggr()
+    val sc = new SparkContext()
+    val views = new PageViewsByLanguage(loadSQLContext(sc)).aggr()
     display(views)
+  }
+  private def loadSQLContext(sc: SparkContext): DataFrame = {
+    val sqlContext = new SQLContext(sc)
+    val df = sqlContext.read
+      .format("com.databricks.spark.csv")
+      .option("header", "false")
+      .option("inferSchema", "false")
+      .option("delimiter", "|")
+      .schema(schema)
+      .load(sc.getConf.get("spark.data.file.location"))
+    df
   }
 
   def display(views: Seq[PageViews]): Unit = {
@@ -15,7 +28,12 @@ object PageViewAnalysis {
     println("-------------------")
   }
 
-  def conf: SparkConf = {
-    new SparkConf().setAppName("page-view-analysis")
+  private def schema(): StructType = {
+    StructType(List(
+      new StructField("language", StringType),
+      new StructField("page", StringType),
+      new StructField("views", IntegerType),
+      new StructField("bytes_transferred", LongType)))
   }
+
 }
--- src/main/scala/com/nbs/PageViewsByLanguage.scala
+++ src/main/scala/com/nbs/PageViewsByLanguage.scala
@@ -2,27 +2,23 @@ package com.nbs
 
 import java.net.URLDecoder
 
-import org.apache.spark.SparkContext
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.types._
-import org.apache.spark.sql.{Row, DataFrame, SQLContext}
+import org.apache.spark.sql.{DataFrame, Row, SQLContext}
 
-case class PageViews(language: String, page: String, count: Long) {
+case class PageViews(language: String, page: String, count: Long) extends Serializable {
   //url decoder is here -- should this be at the source?
   override def toString: String = s"$language | ${URLDecoder.decode(page, "UTF-8")} | $count"
 }
 
-class PageViewsByLanguage(sc: SparkContext) extends Serializable {
-  val sqlContext = new SQLContext(sc)
+class PageViewsByLanguage(df: DataFrame) extends Serializable {
 
   def aggr(): Seq[PageViews] = {
-    val df = load(sqlContext)
     aggregatePageViews(df).registerTempTable("aggr_page_views")
-    top10ForEachLanguage(sqlContext, df)
+    top10ForEachLanguage(df)
   }
 
-  def top10ForEachLanguage(sqLContext: SQLContext, df: DataFrame): Seq[PageViews] = {
-    allLanguages(sqlContext).flatMap(l => sqlContext.sql("select language, page, views from aggr_page_views")
+  def top10ForEachLanguage(df: DataFrame): Seq[PageViews] = {
+    allLanguages(df.sqlContext).flatMap(l => df.sqlContext.sql("select language, page, views from aggr_page_views")
       .where(df("language").equalTo(l)).limit(10)
       .map(toPageView(_))
       .collect())
@@ -32,26 +28,6 @@ class PageViewsByLanguage(sc: SparkContext) extends Serializable {
     new PageViews(language = r.getString(0),
       page = r.getString(1),
       count = r.getLong(2))
-  }
-
-  private def schema(): StructType = {
-    StructType(List(
-      new StructField("language", StringType),
-      new StructField("page", StringType),
-      new StructField("views", IntegerType),
-      new StructField("bytes_transferred", LongType)))
-  }
-
-
-  private def load(sqlContext: SQLContext): DataFrame = {
-    val df = sqlContext.read
-      .format("com.databricks.spark.csv")
-      .option("header", "false")
-      .option("inferSchema", "false")
-      .option("delimiter", "|")
-      .schema(schema)
-      .load(sc.getConf.get("file.location"))
-    df
   }
 
   private def aggregatePageViews(df: DataFrame): DataFrame = {
